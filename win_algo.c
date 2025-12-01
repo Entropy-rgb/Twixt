f9ec921b0779081f579b3f8ad51d54c2a4291da9
Entropy-rgb <som3shkamad@gmail.com>
2025-12-01 12:58:54 +0530

    bug fixes

--- win_algo.c
+++ win_algo.c
@@ -20,7 +20,7 @@ int traverse_red(char matrix[22][22] , int visited_matrix[22][22], int x , int y
         for(int i = 0 ; i < 8 ; i++){
             int nx = x + knight_moves[i][0];
             int ny = y + knight_moves[i][1];
-            if(nx >= 0 && ny >= 0 && nx < 22 && ny < 22 && visited_matrix[x][y] == 0 && matrix[nx][ny] == 'R'){
+            if(nx >= 0 && ny >= 0 && nx < 22 && ny < 22 && visited_matrix[nx][ny] == 0 && matrix[nx][ny] == 'R'){
                 for(int j = 0 ; j < link_count ; j++){
                     if(((x==links[j].a.x && y == links[j].a.y && nx == links[j].b.x && links[j].b.y) || (x == links[j].b.x && y == links[j].b.y && nx == links[j].a.x && links[j].a.y))){
                         ans += traverse_red(matrix, visited_matrix, nx, ny);
@@ -45,7 +45,7 @@ int traverse_black(char matrix[22][22] , int visited_matrix[22][22], int x , int
         for(int i = 0 ; i < 8 ; i++){
             int nx = x + knight_moves[i][0];
             int ny = y + knight_moves[i][1];
-            if(nx >= 0 && ny >= 0 && nx < 22 && ny < 22 && visited_matrix[x][y] == 0 && matrix[nx][ny]=='B'){
+            if(nx >= 0 && ny >= 0 && nx < 22 && ny < 22 && visited_matrix[nx][ny] == 0 && matrix[nx][ny]=='B'){
                 for(int j = 0 ; j < link_count ; j++){
                     if(((x==links[j].a.x && y == links[j].a.y && nx == links[j].b.x && links[j].b.y) || (x == links[j].b.x && y == links[j].b.y && nx == links[j].a.x && links[j].a.y))){
                         ans += traverse_red(matrix, visited_matrix, nx, ny);
